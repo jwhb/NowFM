7e539f2b4508636c2e585ac961143412b6fa8c32
JWhy <jwhy@jwhy.de>
2014-07-19 03:17:36 +0200

    Rename $base to $base_url in layout file
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- views/layout.php
+++ views/layout.php
@@ -1,5 +1,5 @@
 <?php
-  $base = Flight::request()->base;
+  $base_url = Flight::request()->base;
 ?><!DOCTYPE html>
 <!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
 <!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
@@ -14,19 +14,19 @@
 <meta name="author" content="<?php echo $page_author; ?>">
 <meta name="viewport"
 	content="width=device-width, initial-scale=1, maximum-scale=1">
-<link rel="stylesheet" href="<?php echo $base; ?>/style/skeleton.css">
-<link rel="stylesheet" href="<?php echo $base; ?>/style/app.css">
+<link rel="stylesheet" href="<?php echo $base_url; ?>/style/skeleton.css">
+<link rel="stylesheet" href="<?php echo $base_url; ?>/style/app.css">
 
 <!--[if lt IE 9]>
   <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
 
-<link rel="shortcut icon" href="<?php echo $base; ?>/img/favicon.ico">
-<link rel="apple-touch-icon" href="<?php echo $base; ?>/img/apple-touch-icon.png">
+<link rel="shortcut icon" href="<?php echo $base_url; ?>/img/favicon.ico">
+<link rel="apple-touch-icon" href="<?php echo $base_url; ?>/img/apple-touch-icon.png">
 <link rel="apple-touch-icon" sizes="72x72"
-	href="<?php echo $base; ?>/img/apple-touch-icon-72x72.png">
+	href="<?php echo $base_url; ?>/img/apple-touch-icon-72x72.png">
 <link rel="apple-touch-icon" sizes="114x114"
-	href="<?php echo $base; ?>/img/apple-touch-icon-114x114.png">
+	href="<?php echo $base_url; ?>/img/apple-touch-icon-114x114.png">
 
 </head>
 <body>
@@ -34,5 +34,6 @@
 <?php echo $body; ?>
 
 	</div>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 </body>
 </html>
